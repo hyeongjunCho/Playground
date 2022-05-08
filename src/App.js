c8908e1b3cc84ad393c00375b3577dc001c386a5
HyeongjunCho <hyeongjun@kaist.ac.kr>
2022-05-08 18:11:47 +0900

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Settings from './pages/Settings';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/settings">Settings</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
